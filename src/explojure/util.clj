9f4e09bb4ff6a11eb76f7dc371fc0d6342e47271
Aaron Johnson <aaronj@instructure.com>
2015-07-04 14:54:55 -0600

    Add various utility functions from some other projects
    project dataframe
     - rand-n
     - random-seq
     - choose
     - maybe?
     - filter-ab
     - where

--- src/explojure/util.clj
+++ src/explojure/util.clj
@@ -29,3 +29,68 @@
       ;; re
       (mapv #(get result %)
             (sort (keys result))))))
+
+(defn rand-n
+  ([n] (rand-n 1 n))
+  ([max n]
+   (vec (repeatedly n #(rand max)))))
+
+(defn random-seq
+  "
+  Return a lazy sequence where each element is a size 1 random sample
+  from coll. The returned sequence will have the same theoretical frequency
+  distribution as coll.
+
+  Performance Notes
+   - processes 10,000,000 in about 3.5 seconds, where (count coll) is 26
+   - processes 10,000,000 in about 6.5 seconds, where (count coll) is 10e6.
+  "
+  [coll]
+  (lazy-seq
+   (cons (rand-nth coll) (random-seq coll))))
+
+(defn choose
+  "Need to allow with/without replacement options."
+  [x n]
+  (let [population (vec x)
+        indices (take n (shuffle (range (count population))))]
+    (mapv population indices)))
+
+(defn maybe?
+  "
+  Written as a predicate function for filter that randomly returns true
+  with a certain probability (default 0.5).
+
+  Arguments
+   - _: placeholder for when maybe? is used as a predicate for a filter
+   - p: change the probability maybe? will return true
+  "
+  ([] (maybe? nil 0.5))
+  ([_] (maybe? nil 0.5))
+  ([_ p] (< (rand) p)))
+
+
+(defn filter-ab
+  "
+  Return the elements of sequence b for which the application of pred to the
+  corresponding elements in sequence b yields truthy results.
+
+  If pred is not given, it defaults to \"identity,\" which means that for
+  every element in a that is truthy, the corresponding element of b will be
+  returned.
+  "
+  ([a b] (filter-ab identity a b))
+  ([pred a b]
+   (map #(second %)
+        (filter #(pred (first %))
+                (map vector a b)))))
+
+(defn where
+  "
+  Return the 0-based indices where coll is truthy.
+
+  Notes
+   - processes 10,000,000 in approximately 23 seconds
+  "
+  [coll]
+  (filter-ab coll (range (count coll))))
