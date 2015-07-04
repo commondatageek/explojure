e4a053d0519c090eba13b42e9eddf768db10b26e
Aaron Johnson <aaronj@instructure.com>
2015-07-04 14:23:48 -0600

    Add write to CSV functionality

--- src/explojure/io.clj
+++ src/explojure/io.clj
@@ -1,12 +1,21 @@
 (ns explojure.io
-  (require [explojure.util :as util]
+  (require [explojure.core :as core]
+           [explojure.util :as util]
+           
            [clojure.data.csv :as csv]
-           [explojure.core :as core]))
+           [clojure.java.io :as io]))
 
 (defn read-csv [f]
   ;; for now, assuming at least that each CSV file has a header row
-  (with-open [reader (clojure.java.io/reader f)]
+  (with-open [reader (io/reader f)]
     (let [rows (doall (csv/read-csv reader))
           header (first rows)
           data (util/t (next rows))]
       (core/->DataFrame (apply array-map (interleave header data))))))
+
+(defn write-csv [df f]
+  (with-open [writer (io/writer f)]
+    (let [header (core/$colnames tmp)
+          rows (util/t (map #(core/$col df %) header))]
+      (csv/write-csv writer (concat [header] rows)))))
+
