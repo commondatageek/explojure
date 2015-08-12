1ff43ae494dae2e4b0cee768fb277433b39df851
Aaron Johnson <aaronj@instructure.com>
2015-08-11 23:32:41 -0600

    Switch to Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/aaronj1331/explojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.csv "0.1.2"]]
   :plugins [[cider/cider-nrepl "0.9.1"]]
   :repositories [["local" {:url "file:///analysis/clj/repo"
