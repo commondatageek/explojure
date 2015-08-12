b70198f4bb925af34318a56e5fdf795b7833a212
Aaron Johnson <aaronj@instructure.com>
2015-08-12 10:55:59 -0600

    Implement function to calculate max width for a column

--- src/explojure/print.clj
+++ src/explojure/print.clj
@@ -3,6 +3,14 @@
 (defn sp [x] (str " " x " "))
 (defn qu [x] (str "\"" x "\""))
 
+
+;; find max width of column
+(defmulti width string?)
+(defmethod width true [x] (+ (count x) 2))
+(defmethod width false [x] (count (str x)))
+(defn max-width [xs] (apply max (map width xs)))
+
+;; constrain column width
 (defmulti ensure-width (fn [width x] (string? x)))
 (defmethod ensure-width true [width x]
   (let [str-len (count x)
@@ -22,5 +30,4 @@
         str-repr
         (str (subs str-repr 0 (- width 1)) ">"))
       (str (apply str (repeat pad-len " ")) str-repr))))
-
 
