ae4377fa1590cac4bdf5035fc6b16a40d49f4224
Aaron Johnson <aaronj@instructure.com>
2015-10-29 18:54:56 -0600

    Add dlet macro to utilities

--- src/explojure/util.clj
+++ src/explojure/util.clj
@@ -395,3 +395,10 @@ primarily for side effects."
          false
          (recur (next xs) t))))))
 
+(defmacro dlet [bindings & body]
+  `(let [~@(mapcat (fn [[n v]]
+                     (if (or (vector? n) (map? n))
+                       [n v]
+                       [n v '_ `(println (name '~n) ":" ~v)]))
+                   (partition 2 bindings))]
+        ~@body))
