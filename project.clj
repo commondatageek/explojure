e7ef1da49e8cd37e5af64e146e3085ddd15d210d
Aaron Johnson <aaronj@instructure.com>
2015-11-06 22:26:31 +0000

    Move several items to profiles.clj
    - cider-nrepl plugin
    - repositories
    - deploy-repositories

--- project.clj
+++ project.clj
@@ -4,9 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.csv "0.1.2"]]
-  :plugins [[cider/cider-nrepl "0.9.1"]]
-  :repositories [["local" {:url "file:///analysis/clj/repo"
-                           :sign-releases false}]]
-  :deploy-repositories [["local" {:url "file:///analysis/clj/repo"
-                                  :sign-releases false}]])
+                 [org.clojure/data.csv "0.1.2"]])
