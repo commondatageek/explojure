6857245b1bee48a2b25b53eb3c3298701abbba69
Aaron Johnson <aaronj@instructure.com>
2015-11-23 10:24:53 -0700

    Add minor precondition logic to several functions

--- src/explojure/dataframe/core.clj
+++ src/explojure/dataframe/core.clj
@@ -11,14 +11,47 @@
 ;; Placeholder to allow compilation.
 (declare new-dataframe)
 
-(defn cmb-cols-vt [& args]
+(defn sseq?
+  "Return true if x is a sequence having only sequences (zero ro more) as children"
+  [x]
+  (every? identity
+          (concat [(sequential? x)]
+                  (for [el x]
+                    (sequential? el)))))
+(defn truthy? [x]
+  (if x true false))
+
+(defn falsey? [x]
+  (if x false true))
+
+(defn all? [xs]
+  (reduce #(and %1 %2)
+          xs))
+
+(defn equal-length?
+  "REturn true if every sequence x in xs has the same count. Will cause lazy sequences to be evaluated."
+  [xs]
+  {:pre [(every? sequential? xs)]}
+  (apply = (map count xs)))
+
+(defn cmb-cols-vt
+  [& args]
+  {:pre [(every? sseq? args)
+         (equal-length? args)
+         (every? equal-length? args)]}
   (util/vmap util/vflatten
              (apply util/vmap vector args)))
 
 (defn cmb-cols-hr [& args]
+  {:pre [(every? sseq? args)
+         (every? equal-length? args)]}
   (apply util/vconcat args))
 
 (defn collate-keyvals [keyvals]
+  {:pre [(sseq? keyvals)
+         (or (= (count keyvals) 0)
+             (and (= (count (first keyvals)) 2)
+                  (equal-length? keyvals)))]}
   (reduce (fn [m [k v]]
             (assoc m k (conj (get m k []) v)))
           {}
@@ -639,4 +672,9 @@
                                   combined-rownames)
                    (concat cn-df1-and-common cn-df2-only)
                    (concat rn-df1-and-common rn-df2-only))))))
+
+
+
+
+
 
