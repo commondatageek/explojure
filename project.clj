f00f2dae8ef366d004b72f78b65fd91e2c6cfe49
Aaron Johnson <aaronj@instructure.com>
2015-12-29 13:27:39 -0700

    Add lein-release options to this project.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject explojure "0.5.0"
   :description "A simple, solid library for data munging in Clojure."
   :url "http://github.com/aaronj1331/explojure"
+  :lein-release {:scm :git
+                 :deploy-via :shell
+                 :shell ["lein" "deploy" "local"]}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
