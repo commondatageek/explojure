e2ff161d664a34a484343e8861c918920a7a04bf
Aaron Johnson <aaronj@instructure.com>
2016-01-07 16:59:58 -0700

    Release 0.6.1
    A number of minor bug fixes
     - Fixed some references that were broken by the refactor
     - Removed type hints from explojure.dataframe.impl.* namespaces
     - Made more robust several functions necessary for joins
     - Added unit tests to join namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject explojure "0.6.0"
+(defproject explojure "0.6.1"
   :description "A simple, solid library for data munging in Clojure."
   :url "http://github.com/aaronj1331/explojure"
   :lein-release {:scm :git
