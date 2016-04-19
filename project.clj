53fd5a236329da8e5735c589f2f1bce6d32cd492
Aaron Johnson <aaronj@instructure.com>
2016-04-19 15:42:17 -0600

    Release 0.7.0
    Notable improvements
     - major speed improvements on join-frames
       (outer join 1.2 million on 10 million with 3.5gb ram in 90 seconds)
     - changed get-inner-indices from n-squared to linear time complexity
     - added some new functionality
        - drop-duplicates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject explojure "0.6.3-SNAPSHOT"
+(defproject explojure "0.7.0"
   :description "A simple, solid library for data munging in Clojure."
   :url "http://github.com/aaronj1331/explojure"
   :lein-release {:scm :git
@@ -7,5 +7,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.commons/commons-csv "1.2"]])
+                 [org.apache.commons/commons-csv "1.2"]
+                 [org.apache.commons/commons-math3 "3.6.1"]])
 
